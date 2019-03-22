29aa5b5ea753e2525bed60cbb509aa503b8bc178
Miguel Arconada WIN <miguelarman@gmail.com>
2019-03-22 19:33:26 +0100

    La primera funcion deberia ir ya

--- pra3/pract3a.asm
+++ pra3/pract3a.asm
@@ -5,7 +5,9 @@
 DGROUP GROUP _DATA, _BSS				;; Se agrupan segmentos de datos en uno
 
 _DATA SEGMENT WORD PUBLIC 'DATA' 		;; Segmento de datos DATA público
-
+	SUMA_TOTAL dw 0
+	SUMA_IMPAR dw 0
+	SUMA_PAR dw 0
 _DATA ENDS
 
 _BSS SEGMENT WORD PUBLIC 'BSS'			;; Segmento de datos BSS público
@@ -21,40 +23,47 @@ _computeControlDigit PROC FAR
 
 	; Guarda los valores de los registros para no modificarlos
 	PUSH BP
+	PUSH AX BX CX DX SI ES
+	
+	; Carga el registro para DGROUP
+	MOV AX, DGROUP
+	MOV DS, AX
 	
 	; Guarda en BP el valor de SP
 	MOV BP, SP
 
 	; Lee la direccion de la entrada
+	LES BX, [BP + 6]				; BX contiene el offset del dato de entrada
+									; ES contiene el segmento
 
 	; Prepara los registros necesarios para el bucle
-	MOV SUMA_TOTAL, 0
-	MOV SUMA_IMPAR, 0
-	MOV SUMA_PAR, 0
-	MOV CONTADOR, 0
+	MOV SI, 0
 
 BUCLE_SUMA:
-	MOV U, CODIGO[CONTADOR]
-	SUB U, 30				; Convertimos de ascii a binario
+	MOV DX, ES:BX[SI]			; Carga en DX un digito del codigo de barras
+	SUB DX, 30					; Convertimos de ascii a binario
 	
 	; Opera con el digito
-	TEST CONTADOR, 00000001b ; Comprueba si es par (impar al empezar en cero)
+	TEST SI, 00000001b			; Comprueba si es par (impar al empezar en cero)
 	JNZ ES_PAR
 	
-	ADD SUMA_IMPAR, U		; Es impar
+	ADD SUMA_IMPAR, DX			; Es impar
 	JMP CONTINUA_SUMA
 ES_PAR:
-	ADD SUMA_PAR, U			; Es par
+	ADD SUMA_PAR, DX			; Es par
 	
 CONTINUA_SUMA:
-	INC CONTADOR
-	CMP CONTADOR, 12
+	INC SI
+	CMP SI, 12
 	JNE BUCLE_SUMA
 	
-	; Sale del bucle
-	MUL SUMA_PAR, 3
-	ADD SUMA_TOTAL, SUMA_PAR
-	ADD SUMA_TOTAL, SUMA_IMPAR
+	; Sale del bucle. Calcula la suma
+	MOV AX, 3
+	MOV CX, SUMA_PAR
+	MUL CX
+	MOV DX, SUMA_IMPAR
+	ADD AX, DX
+	MOV SUMA_TOTAL, AX
 	
 	; Calcula el digito de control
 	MOV AX, SUMA_TOTAL
@@ -67,7 +76,9 @@ CONTINUA_SUMA:
 	
 	
 	
-	; Guarda los valores de los registros para no modificarlo
+	; Recupera los valores anteriores de los registros
+	POP ES SI DX CX BX AX
+	POP BP
 	RET
 
 
