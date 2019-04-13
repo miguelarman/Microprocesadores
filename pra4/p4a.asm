f905a35f92049e86a007b2eee87711b84f498b1b
Miguel Arconada WIN <miguelarman@gmail.com>
2019-04-13 09:10:48 +0200

    Segyumos cin el driver

--- pra4/p4a.asm
+++ pra4/p4a.asm
@@ -5,35 +5,52 @@ codigo SEGMENT
 inicio: jmp lee_argumentos
 
 ; Variables globales
-polibio						db	3, 4, 5, 6, 7, 8, 9, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 1, 2
-cadena_aux					db	32 dup (?)
-mensaje_informacion			db "Informacion$"
+polibio									db	3, 4, 5, 6, 7, 8, 9, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 1, 2
+cadena_aux								db	32 dup (?)
+mensaje_informacion_general				db	"Informacion acerca de este programa:", 0Ah
+										db	9h, "Llamar a esta funcion con el argumento /I para instalar el driver", 0Ah
+										db	9h, "Llamar a esta funcion con el argumento /D para desinstalar el driver", 0Ah
+										db	9h, "Una vez instalado, este programa gestiona la interrupcion 57h,", 0Ah
+										db	9h, "cifrando o descifrando con Polibio dependiendo del valor en AH", 0Ah
+										db	9h, "(10h para cifrar y 11h para descifrar)", 0Ah
+										db	0Ah, "Numero de pareja:", 9h, "9", 0Ah
+										db	0Ah, "Ejercicio desarrollado por:", 0Ah
+										db	9h, "Miguel Arconada", 0Ah
+										db	9h, "Mario Garcia", 0Ah
+										db	"$"
+mensaje_informacion_instalador			db "Se ha ejecutado el instalador$"
+mensaje_informacion_desinstalador		db "Se ha ejecutado el desinstalador$"
 mensaje_error_parametros_num_caracteres	db	"Error en los parametros: Escribe solo dos caracteres$"
 mensaje_error_parametros_otro_caracter	db	"Error en los parametros: El parametro tiene que ser /I o /D$"
-mensaje_error_parametros_sin_barra	db	"Error en los parametros: El parametro tiene que empezar por /$"
-clear_pantalla				db 1BH,"[2","J$"
+mensaje_error_parametros_sin_barra		db	"Error en los parametros: El parametro tiene que empezar por /$"
+mensaje_debug							db	0Ah,"Debugueando$"
+clear_pantalla							db	1BH,"[2","J$"
+mensaje_instalado						db	0Ah,"El driver esta instalado$"
+mensaje_no_instalado					db	0Ah,"El driver no esta instalado$"
 
+firma									dw	0ACABh
 
 ; Rutina de servicio a la interrupción
 rsi PROC FAR
 	; Salva registros modificados
-	; push ...
+	push ax
 	
 	; Instrucciones de la rutina
-	; ...
+	mov ax, 0
 	
 	; Recupera registros modificados
-	; pop ...
+	pop ax
 	iret
 	
 lee_argumentos:
+
 	; Lee del PSP el tamaño en bytes de los parámetros del programa
 	mov al, es:[80h]
-	
+
 	; Si recibe 0 caracteres, muestra información por pantalla
 	cmp al, 0
 	jz informacion
-	
+
 	; Si recibe argumentos, tienen que ser tres caracteres
 	cmp al, 3
 	jne error_parametros_num_caracteres
@@ -58,42 +75,107 @@ lee_argumentos:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 informacion:
-	; Limpia la pantalla
-	lea dx, clear_pantalla
-	mov ah, 9
-	int 21h
-	
-	; Imprime el mensaje de informacion
-	mov ah, 9
-	mov dx, OFFSET mensaje_informacion
-	int 21h
+	call rutina_informacion
 	jmp fin_ejecucion_normal
 
 error_parametros_num_caracteres:
+	call rutina_error_parametros_num_caracteres
+	jmp fin_ejecucion_normal
+	
+error_parametros_otro_caracter:
+	call rutina_error_parametros_otro_caracter
+	jmp fin_ejecucion_normal
+
+error_parametros_sin_barra:
+	call rutina_error_parametros_sin_barra
+	jmp fin_ejecucion_normal
+
+desinstalador:
+	call rutina_desinstalador
+	jmp fin_ejecucion_normal
+	
+instalador:
 	; Limpia la pantalla
-	lea dx, clear_pantalla
+	call limpia_pantalla
+	
+	; Imprime el mensaje de informacion del desinstalador
 	mov ah, 9
-	INT 21H
+	mov dx, OFFSET mensaje_informacion_instalador
+	int 21h
+
+	mov ax, 0
+	mov es, ax
+	mov ax, OFFSET rsi
+	mov bx, cs
+	
+	cli
+	mov es:[57h*4], ax
+	mov es:[57h*4+2], bx
+	sti
+	
+	mov dx, OFFSET instalador
+	int 27h	; Acaba y deja residente
+			; PSP, variables y rutina rsi.
+	
+fin_ejecucion_normal:
+	mov ax, 4C00h
+	int 21h
+rsi ENDP
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                RUTINAS AUXILIARES               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+rutina_informacion PROC
+	push ax dx
+	
+	; Limpia la pantalla
+	call limpia_pantalla
+	
+	; Imprime el mensaje de informacion
+	mov ah, 9
+	mov dx, OFFSET mensaje_informacion_general
+	int 21h
+	
+	call rutina_mensaje_instalado
+	
+	pop dx ax
+	ret
+rutina_informacion ENDP
+
+rutina_error_parametros_num_caracteres PROC
+	push ax dx
+	
+	; Limpia la pantalla
+	call limpia_pantalla
 	
 	; Imprime el mensaje de informacion
 	mov ah, 9
 	mov dx, OFFSET mensaje_error_parametros_num_caracteres
 	int 21h
-	jmp fin_ejecucion_normal
 	
-error_parametros_otro_caracter:
+	pop dx ax
+	ret
+rutina_error_parametros_num_caracteres ENDP
+
+rutina_error_parametros_otro_caracter PROC
+	push ax dx
+	
 	; Limpia la pantalla
-	lea dx, clear_pantalla
-	mov ah, 9
-	INT 21H
+	call limpia_pantalla
 	
 	; Imprime el mensaje de informacion
 	mov ah, 9
 	mov dx, OFFSET mensaje_error_parametros_otro_caracter
 	int 21h
-	jmp fin_ejecucion_normal
+	
+	pop dx ax
+	ret
+rutina_error_parametros_otro_caracter ENDP
 
-error_parametros_sin_barra:
+rutina_error_parametros_sin_barra PROC
+	push ax dx
+	
 	; Limpia la pantalla
 	lea dx, clear_pantalla
 	mov ah, 9
@@ -103,29 +185,126 @@ error_parametros_sin_barra:
 	mov ah, 9
 	mov dx, OFFSET mensaje_error_parametros_sin_barra
 	int 21h
-	jmp fin_ejecucion_normal
+	
+	pop dx ax
+	ret
+rutina_error_parametros_sin_barra ENDP
 
-desinstalador:
-	jmp fin_ejecucion_normal
+rutina_desinstalador PROC
+	push ax cx dx
+
+	; Limpia la pantalla
+	call limpia_pantalla
 	
-	
-instalador:
-	mov ax, 0
-	mov es, ax
-	mov ax, OFFSET rsi
-	mov bx, cs
-	cli
-	mov es:[57h*4], ax
-	mov es:[57h*4+2], bx
-	sti
-	mov dx, OFFSET instalador
-	int 27h	; Acaba y deja residente
-			; PSP, variables y rutina rsi.
-	
-fin_ejecucion_normal:
-	mov ax, 4C00h
+	; Imprime el mensaje de informacion del desinstalador
+	mov ah, 9
+	mov dx, OFFSET mensaje_informacion_desinstalador
 	int 21h
-rsi ENDP
+	
+	; Comprueba el vector INT
+	cmp ds:[57h*4], WORD PTR 0
+	jz no_instalado_desinstalar
+	cmp ds:[57h*4+2], WORD PTR 0
+	jz no_instalado_desinstalar
+	
+	; Comprueba si está instalado (firma)
+	call comprueba_instalado
+	cmp ax, 0
+	je no_instalado_desinstalar
+	
+	; Desinstala el driver
+	desinstalar_57h:
+	mov cx, 0
+	mov ds, cx				; Segmento de vectores interrupción
+	mov es, ds:[57h*4+2]	; Lee segmento de RSI
+	mov bx, es:[2Ch]		; Lee segmento de entorno del PSP de RSI
+	mov ah, 49h
+	int 21h					; Libera segmento de RSI (es)
+	mov es, bx
+	int 21h					; Libera segmento de variables de entorno de RSI
+	
+	; Pone a cero vector de interrupción 57h
+	cli
+	mov ds:[57h*4], cx 		; cx = 0
+	mov ds:[57h*4+2], cx
+	sti
+	
+	jmp final_desinstalador
+	
+	no_instalado_desinstalar:
+	call rutina_mensaje_instalado
+	
+	final_desinstalador:
+	pop dx cx ax
+	ret
+rutina_desinstalador ENDP
+
+limpia_pantalla PROC
+	push dx ax
+	
+	; Limpia la pantalla
+	lea dx, clear_pantalla
+	mov ah, 9
+	INT 21H
+	
+	pop ax dx
+	ret
+limpia_pantalla ENDP
+
+rutina_mensaje_instalado PROC
+	call comprueba_instalado
+	
+	cmp ax, 1
+	je imprime_mensaje_instalado
+	jmp imprime_mensaje_no_intalado
+	
+	imprime_mensaje_instalado:
+	mov ah, 9
+	mov dx, OFFSET mensaje_instalado
+	int 21h
+	
+	ret
+	
+	imprime_mensaje_no_intalado:
+	mov ah, 9
+	mov dx, OFFSET mensaje_no_instalado
+	int 21h
+	
+	ret
+rutina_mensaje_instalado ENDP
+
+comprueba_instalado PROC
+	mov ax, firma
+	cmp ax, es:[si-2]
+	je iguales
+	jmp distintos
+
+	ret
+	
+	iguales:
+	mov ax, 1
+	jmp final_comprueba_instalado
+
+	distintos:
+	mov ax, 0
+	jmp final_comprueba_instalado
+	
+	final_comprueba_instalado:
+	ret
+	
+comprueba_instalado ENDP
+
+debug PROC
+	push ax dx
+		
+	; Imprime el mensaje de informacion del desinstalador
+	mov ah, 9
+	mov dx, OFFSET mensaje_debug
+	int 21h
+	
+	pop dx ax
+	ret
+debug ENDP
 
 codigo ENDS
 END inicio
